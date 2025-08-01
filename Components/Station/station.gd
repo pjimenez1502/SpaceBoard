1c855d299f8559a5cdc834c4fc280e7d4e8d7147
pjimenez1502 <pjimenez1502@gmail.com>
2025-08-01 11:10:41 +0200

    Lowered Station rotation speed

--- Components/Station/station.gd
+++ Components/Station/station.gd
@@ -1,6 +1,6 @@
 extends Node3D
 
-@export var rotation_speed : float = 0.2
+@export var rotation_speed : float = 0.05
 
 func _physics_process(delta: float) -> void:
 	rotate_object_local(Vector3.UP, rotation_speed * delta)
